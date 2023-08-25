b14645aad7eb5494809eadf837e11c1e72b192c7
manuel <manuelhpineda2000@gmail.com>
2023-08-25 13:57:20 -0600

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.liquid', './src/**/*.{ts,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import fg from 'fast-glob'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'assets',
+    emptyOutDir: false,
+    minify: false,
+    rollupOptions: {
+      input: 'src/**/*.{ts,js,css}',
+      output: {
+        dir: 'assets',
+        entryFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+      plugins: [
+        {
+          name: 'glob-input',
+          options(options) {
+            const inputs = typeof options.input === 'string' ? [options.input] : options.input
+
+            return Array.isArray(inputs)
+              ? { ...options, input: inputs.flatMap((input) => fg.sync(input)) }
+              : null
+          },
+        },
+      ],
+    },
+  },
+  plugins: [],
+})
